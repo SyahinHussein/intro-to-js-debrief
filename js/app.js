8b51edd44746a3c5de652d2261e044bd5458b7d1
SyahinHussein <in.syahin2@gmail.com>
2021-01-25 21:57:40 -0800

    I have made an edit to a file!

---
+++ js/app.js
@@ -0,0 +1,64 @@
+// 1. GenBuzz 
+//    * Create variable `myNumber`
+//         * Check that the value of the `myNumber` is of type `number`
+//    * If `myNumber` is not a number, print 'This is not a number' to the console.
+//    * If value of `myNumber` is a multiple of 3 print 'Gen' to the console
+//    * If value of `myNumber` is a multiple of 5 print 'Buzz' to the console
+//    * If the value of `myNumber` is a multiple of both 3 and 5 (ex. 15) print GenBuzz to the console
+//    * If the value of `myNumber` is any other number print the value of `myNumber` to the console.
+//    * Reference Codecademy lessons: Introduction- Arithmetic Opertaors, Conditional Statements lessons 3, 4, 5,and 9 
+        let myNumber=10;
+        for(let myNumber =1; myNumber <100; myNumber++) {
+        if(typeof myNumber !== 'number'){
+            console.log('This is not a number');
+        }else if(myNumber%15 === 0){
+            console.log('GenBuzz');
+        }else if(myNumber%3 === 0){
+            console.log('Gen');
+        }else if (myNumber%5 === 0){
+            console.log('Buzz');
+        }else{
+            console.log(myNumber);
+        }
+    }
+
+
+// 2. E-Commerce Item list
+//     * Use a swtich statement to print the price of the each item in the store to the console.
+//     * Ex. If the value of the variable is 'shoes' then, the statement `Shoes are $50` should be printed to the console. 
+//     * Reference: Codecademy lesson 10
+
+//     Items: 
+//      * Shoes- $50
+//      * Jeans- $25
+//      * Hat- $12
+//      * Socks- $2
+//      * If the variable input is not an item in the store, then print 'Invalid Item' to the console.
+
+
+    let priceOfEachItem = 'Socks';
+    switch(priceOfEachItem){
+        case 'Shoes':
+        return console.log('Shoes are $50');
+        break;
+        case 'Jeans':
+        return console.log('Jeans are $25');
+        break;
+        case 'Hat':
+        return console.log('Hats are $12');
+        break;
+        case 'Socks':
+        return console.log('Socks are $2');
+        break;
+        default:
+        console.log('Invalid Item');
+    }
+
+// 3. Print a random integer (whole number) between 50 and 100  to the console
+//     * Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+function PrintRandomInteger(min, max) {
+    return console.log((Math.floor(Math.random() * (max - min)) + min));
+}
+
+PrintRandomInteger(50,100);
